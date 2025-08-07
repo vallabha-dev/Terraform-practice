8ea054e12d0b4b208971f0a7894ddbd456b5d502
veera <veerababu.narni232@gmail.com>
2025-08-07 10:20:56 +0530

    updates

--- Day-4-statelocking/backend.tf
+++ Day-4-statelocking/backend.tf
@@ -3,6 +3,8 @@ terraform {
     bucket = "testedtetetst"
     key    = "day-4/terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
+    #use_lockfile = true #s3 supports this feature but teraaform version > 1.10, latest version >=1.10
+    dynamodb_table = "test"  #any version 
+    encrypt = true
   }
 }
--- Day-4-statelocking/main.tf
+++ Day-4-statelocking/main.tf
@@ -2,14 +2,14 @@ resource "aws_instance" "name" {
     ami = "ami-08a6efd148b1f7504"
     instance_type = "t2.nano"
     tags = {
-      Name = "dev-1"
+      Name = "test"
     }
   
 }
 resource "aws_vpc" "name" {
     cidr_block = "10.0.0.0/16"
     tags = {
-      Name = "devvpc"
+      Name = "devnewtest"
     }
   
 }
